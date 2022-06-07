6bda55ed77e1bfe817a11fc1399c0a3e99e644f0
badje <badjemm@3il.fr>
2022-06-07 23:37:37 +0200

    add Fake MDP contratc and policy evaluation function
    Signed-off-by: badje <badjemm@3il.fr>

--- drl_lib/to_do/dynamic_programming.py
+++ drl_lib/to_do/dynamic_programming.py
@@ -1,15 +1,63 @@
 from ..do_not_touch.mdp_env_wrapper import Env1
 from ..do_not_touch.result_structures import ValueFunction, PolicyAndValueFunction
+from .MDP_contracts import MyMDPEnv
+import numpy as np
 
 
 def policy_evaluation_on_line_world() -> ValueFunction:
     """
     Creates a Line World of 7 cells (leftmost and rightmost are terminal, with -1 and 1 reward respectively)
     Launches a Policy Evaluation Algorithm in order to find the Value Function of a uniform random policy
-    Returns the Value function (V(s)) of this policy
+    Returns the Value function (V(s)) of this policy Dict[s:int, V(s):float]
     """
+    nb_cells = 7
+    states = np.arange(nb_cells)
+    actions = np.array([0, 1])
+    rewards = np.array([-1.0, 0.0, 1.0])
+    transition_matrix = np.zeros((len(states), len(actions), len(states), len(rewards)))
+    for s in states[1:-1]:
+        if s == 1:
+            transition_matrix[s, 0, s - 1, 0] = 1.0
+        else:
+            transition_matrix[s, 0, s - 1, 1] = 1.0
+
+        if s == nb_cells - 2:
+            transition_matrix[s, 1, s + 1, 2] = 1.0
+        else:
+            transition_matrix[s, 1, s + 1, 1] = 1.0
+
+    terminal_states = [states[0],states[-1]]
+
+
+    env = MyMDPEnv(states=states,rewards=rewards,actions=actions,terminal_states=terminal_states,transition_matrix=transition_matrix)
+
+
     # TODO
-    pass
+    theta = 0.0000001
+    V = np.random.random((nb_cells,))
+    Vs:ValueFunction = {s:V[s] for s in env.states }
+    Vs[0] = 0.0
+    Vs[nb_cells - 1] = 0.0
+
+    pi = np.random.random((nb_cells, (len(env.actions))))
+
+    while True:
+        delta = 0
+        for s in env.states:
+            v = Vs[s]
+            Vs[s] = 0.0
+            for a in env.actions:
+                total = 0.0
+                for s_p in env.states:
+                    for r in range(len(env.rewards)):
+                        total += env.transition_probability(s, a, s_p, r) * (env.rewards[r] + 0.999 * Vs[s_p])
+                total *= pi[s, a]
+                Vs[s] += total
+            delta = max(delta, np.abs(v - Vs[s]))
+        if delta < theta:
+            break
+
+    return Vs
 
 
 def policy_iteration_on_line_world() -> PolicyAndValueFunction:
@@ -97,13 +145,13 @@ def value_iteration_on_secret_env1() -> PolicyAndValueFunction:
 
 def demo():
     print(policy_evaluation_on_line_world())
-    print(policy_iteration_on_line_world())
-    print(value_iteration_on_line_world())
+    # print(policy_iteration_on_line_world())
+    # print(value_iteration_on_line_world())
 
-    print(policy_evaluation_on_grid_world())
-    print(policy_iteration_on_grid_world())
-    print(value_iteration_on_grid_world())
+    # print(policy_evaluation_on_grid_world())
+    # print(policy_iteration_on_grid_world())
+    # print(value_iteration_on_grid_world())
 
-    print(policy_evaluation_on_secret_env1())
-    print(policy_iteration_on_secret_env1())
-    print(value_iteration_on_secret_env1())
+    # print(policy_evaluation_on_secret_env1())
+    # print(policy_iteration_on_secret_env1())
+    # print(value_iteration_on_secret_env1())
