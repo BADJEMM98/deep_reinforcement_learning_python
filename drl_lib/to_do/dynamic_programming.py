57c52931921d4ce8c20c88dd3b4156ad315c116b
badje <badjemm@3il.fr>
2022-06-12 01:09:36 +0200

    utils.py and add env_data in MDP_contrats
    Signed-off-by: badje <badjemm@3il.fr>

--- drl_lib/to_do/dynamic_programming.py
+++ drl_lib/to_do/dynamic_programming.py
@@ -3,25 +3,8 @@ from ..do_not_touch.mdp_env_wrapper import Env1
 from ..do_not_touch.result_structures import ValueFunction, PolicyAndValueFunction
 from .MDP_contracts import MyMDPEnv
 import numpy as np
+from .utils import *
 
-def policy_eval(env, pi, Vs, theta=0.0000001):
-    while True:
-        delta = 0
-        for s in env.states():
-            v = Vs[s]
-            Vs[s] = 0.0
-            for a in env.actions():
-                total = 0.0
-                for s_p in env.states():
-                    for r in range(len(env.rewards())):
-                        total += env.transition_probability(s, a, s_p, r) * (env.rewards()[r] + 0.999 * Vs[s_p])
-                total *= pi[s][a]
-                Vs[s] += total
-            delta = max(delta, np.abs(v - Vs[s]))
-
-        if delta < theta:
-            break
-    return Vs
 
 
 def policy_evaluation_on_line_world() -> ValueFunction:
@@ -47,23 +30,29 @@ def policy_evaluation_on_line_world() -> ValueFunction:
             transition_matrix[s, 1, s + 1, 1] = 1.0
 
     terminal_states = [states[0],states[-1]]
+    env_data = {
+        "states":states,
+        "actions":actions,
+        "rewards":rewards,
+        "terminal_states":terminal_states,
+        "transition_matrix":transition_matrix
+    }
 
-
-    env = MyMDPEnv(states=states,rewards=rewards,actions=actions,terminal_states=terminal_states,transition_matrix=transition_matrix)
+    env = MyMDPEnv(env_data)
 
 
     # TODO
     theta = 0.0000001
     V = np.random.random((nb_cells,))
-    Vs:ValueFunction = {s:V[s] for s in env.states }
+    Vs:ValueFunction = {s:V[s] for s in env.states() }
     Vs[0] = 0.0
     Vs[nb_cells - 1] = 0.0
 
-    pi = {s:{a:random() for a in env.actions} for s in env.states}
-    for s in env.states:
+    pi = {s:{a:random() for a in env.actions()} for s in env.states()}
+    for s in env.states():
         pi[s] = {a:v/total for total in (sum(pi[s].values()),) for a, v in pi[s].items()}
-    pi[0] = {a:0.0 for a in env.actions}
-    pi[nb_cells - 1] = {a:0.0 for a in env.actions}
+    pi[0] = {a:0.0 for a in env.actions()}
+    pi[nb_cells - 1] = {a:0.0 for a in env.actions()}
 
     Vs = policy_eval(env, pi, Vs, theta=theta)
 
@@ -89,44 +78,6 @@ def value_iteration_on_line_world() -> PolicyAndValueFunction:
     # TODO
     pass
 
-# Initialisation de transition_matrix
-def init_grid_transition(grid_size, states, actions, rewards):
-    transition_matrix = np.zeros((len(states), len(actions), len(states), len(rewards)))
-    for s in states:
-        # Les movements verticaux
-        # Direction haut
-        if (int(s / grid_size) == 1) & (s % grid_size == 4):
-            transition_matrix[s, 0, s - grid_size, 0] = 1.0
-        elif int(s / grid_size) > 0:
-            transition_matrix[s, 0, s - grid_size, 1] = 1.0
-
-        # Direction bas
-
-        if (int(s / grid_size) == grid_size - 2) & (s % grid_size == 4):
-            transition_matrix[s, 1, s + grid_size, 2] = 1.0
-        elif int(s / grid_size) < 4:
-            transition_matrix[s, 1, s + grid_size, 1] = 1.0
-
-        # Les movements de horizontaux
-        # Direction gauche
-        if s % grid_size > 0:
-            transition_matrix[s, 2, s - 1, 1] = 1.0
-
-        # Direction droite
-        if s == grid_size - 2:
-            transition_matrix[s, 3, s + 1, 0] = 1.0
-        elif s == grid_size * grid_size - 2:
-            transition_matrix[s, 3, s + 1, 2] = 1.0
-        elif s % grid_size < 4:
-            transition_matrix[s, 3, s + 1, 1] = 1.0
-
-    for s_p in states:
-        for a in actions:
-            for r in range(len(rewards)):
-                transition_matrix[grid_size - 1, a, s_p, r] = 0.0
-                transition_matrix[grid_size * grid_size - 1, a, s_p, r] = 0.0
-
-    return transition_matrix
 
 def policy_evaluation_on_grid_world() -> ValueFunction:
     """
@@ -242,7 +193,7 @@ def value_iteration_on_secret_env1() -> PolicyAndValueFunction:
 
 
 def demo():
-    # print(policy_evaluation_on_line_world())
+    print(policy_evaluation_on_line_world())
     # print(policy_iteration_on_line_world())
     # print(value_iteration_on_line_world())
 
@@ -250,6 +201,6 @@ def demo():
     # print(policy_iteration_on_grid_world())
     # print(value_iteration_on_grid_world())
 
-    print(policy_evaluation_on_secret_env1())
+    # print(policy_evaluation_on_secret_env1())
     # print(policy_iteration_on_secret_env1())
     # print(value_iteration_on_secret_env1())
