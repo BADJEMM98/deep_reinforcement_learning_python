9a54b0bdb99b52d04c1df16523245f4f6ed9dd33
badje <badjemm@3il.fr>
2022-06-24 23:27:00 +0200

    update monte carlo es
    Signed-off-by: badje <badjemm@3il.fr>

--- drl_lib/to_do/Player.py
+++ drl_lib/to_do/Player.py
@@ -8,15 +8,15 @@ class Player():
         self.is_winner = False
         self.type = type # 'H':human player, 'R': Random player, 'A'
 
-    def play(self,available_actions_ids,state_id=None,policy=None) -> Optional[int]:
+    def play(self,available_actions,state_id=None,policy=None) -> Optional[int]:
         action_id=None
         if self.type == 'H':
             while True:
                 action_id = input("Please enter your action id: ")
-                if action_id in available_actions_ids():
+                if action_id in available_actions:
                     break
         elif self.type == 'R':
-            action_id = np.random.choice(available_actions_ids)
+            action_id = np.random.choice(available_actions)
         else:
             if state_id is not None and policy is not None:
                 action_id = max(policy[state_id],key=policy[state_id].get)
